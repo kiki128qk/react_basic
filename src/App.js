5a459882bab40bfb61a9cabf1360701e9b1223e3
jung_jung <kiki128qk@naver.com>
2021-04-09 18:39:15 +0900

    react basic 마무리

--- src/App.js
+++ src/App.js
@@ -1,51 +1,22 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./components/Movie";
-class App extends React.Component{
+// import axios from "axios";
+// import Movie from "./components/Movie";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import  "./App.css";
+import {HashRouter, Route} from 'react-router-dom';
 
-state ={
-  isLoading: true,
-  movies:[],
-};
-
-getMovies= async()=>{
-  const {
-    data:{
-      data:{movies}
-    }
-  } = await axios.get('https://yts.mx/api/v2/list_movies.json');
-  // console.log(movies);
-
-  this.setState({isLoading:false, movies:movies});
-}
-
-componentDidMount(){
-  // setTimeout(function(){
-  //   this.setState({isLoading:false});
-  // }, 3000);
-  // setTimeout(()=>{
-  //   this.setState({isLoading: false});
-  // },3000);
- this.getMovies();   
-}
-
-render(){
-
-  //const isLoading =this.state.isLoading;
-  const{isLoading, movies}=this.state;
-  
-  return(
- 
-    <div>
-     {isLoading ? "loading..." : movies.map((movie)=>{
-       console.log(movie);
-       return(<Movie key={movie.id} title={movie.title} year={movie.year} rating={movie.rating} />
-        );
-     })}
-    </div>
+function App(){
+  return (
+  <HashRouter>
+    <Navigation/>
+    <Route path="/" exact={true} component={Home}></Route>
+    <Route path="/about" component={About}></Route>
+    <Route path="/movie_detail" component={Detail}></Route>
+  </HashRouter>
   );
+ 
 }
-
-}
-
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-function Movie({title, year,rating}){
+import  "./Movie.css";
+import {Link}from 'react-router-dom'
+function Movie({title, year,summary, poster, genres}){
   return (
-  <div>
-    <h4>{title}</h4>
-    <h3>{year}</h3>
-    <em>{rating}</em>
+  <div className='moveie'>
+    <Link to={{pathname:'/movie_detail', state:{year, title, summary, poster, genres}}}>
+    <img src={poster} alt={title} onError={(e)=>{e.target.onerror = null; e.target.src="https://developers.google.com/maps/documentation/streetview/images/error-image-generic.png"}}/>
+    <div class="movie_data">
+      <h3 class="movie_title">{title}</h3>
+      <h5 class="movie_year">{year}</h5>
+      <ul>
+        {genres.map((genre , idx)=>{
+          return(
+            <li key={idx}>{genre}</li>
+          )
+        })}
+      </ul>
+      <p className="movie_summary">{summary}</p>
+    </div>
+    </Link>
   </div>);
 }
 
 Movie.propType={
   title:PropTypes.string.isRequired,
   year:PropTypes.number.isRequired,
-  nating:PropTypes.string.isRequired,
+  summary:PropTypes.string.isRequired,
+  poster:PropTypes.string.isRequired,
+  genres:PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 export default Movie;
